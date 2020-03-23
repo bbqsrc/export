3080dfb28e5686cc531040d1c39be43bc191e3db
Brendan Molloy <brendan@bbqsrc.net>
2020-03-23 23:51:10 +0100

    Oh yes, it does more now

--- export_macro/src/lib.rs
+++ export_macro/src/lib.rs
@@ -1,9 +1,125 @@
 use proc_macro2::TokenStream;
 use syn::spanned::Spanned;
-use syn::{Error, Item, ItemFn, Visibility};
+use syn::{AttrStyle, Error, Item, ItemFn, ItemMod, Visibility, Meta, Lit};
 use quote::quote;
 
-// Todo: implement an internal variant
+#[ctor::ctor]
+fn init() {
+    env_logger::init();
+}
+
+const SCRIPT_MOD_FN: &str = r#"<script>
+if (document.body.classList.contains('mod')) {{
+    var s = document.currentScript;
+    if (s.parentNode.parentNode.id != 'main') {
+        var n = s.parentNode.previousElementSibling;
+        console.log(n)
+        n.classList.remove('deprecated');
+        n.classList.add('unstable');
+        n.innerText = 'Unstable';
+    }
+}}
+</script>
+"#;
+
+
+const SCRIPT_MOD: &str = r#"<div class="stability export-unstable">
+<div class="stab unstable">
+<details>
+<summary>
+<span class="emoji">🔬</span>
+This module is <strong>unstable</strong>, and requires the feature <code>unstable</code> to be enabled.
+</summary>
+</details>
+</div>
+</div>
+<script>
+(function(){{
+if (document.body.classList.contains('mod')){{
+  var s = document.currentScript;
+  var u = s.previousElementSibling;
+  var x = s.parentNode.previousElementSibling;
+  x.parentNode.replaceChild(u, x);
+}}
+}})()
+</script>"#;
+
+const SCRIPT_FN: &str = r#"<div class="stability export-unstable">
+<div class="stab unstable">
+<details>
+<summary>
+<span class="emoji">🔬</span>
+This function is <strong>unstable</strong>, and requires the feature <code>unstable</code> to be enabled.
+</summary>
+</details>
+</div>
+</div>
+<script>
+(function(){{
+if (document.body.classList.contains('fn')){{
+  var s = document.currentScript;
+  var u = s.previousElementSibling;
+  var x = s.parentNode.previousElementSibling;
+  x.parentNode.replaceChild(u, x);
+}}
+}})()
+</script>"#;
+
+fn unstable_item_mod(item_mod: ItemMod) -> Result<TokenStream, syn::Error> {
+    match item_mod.vis {
+        Visibility::Public(_) => return Err(Error::new(item_mod.span(), "function is already public, cannot be unstable")),
+        _ => {}
+    };
+
+    let mut pub_item_mod = item_mod.clone();
+    pub_item_mod.vis = Visibility::Public(syn::VisPublic { pub_token: syn::token::Pub::default() });
+
+    let mut doc_input: Vec<String> = vec![];
+
+    pub_item_mod.attrs = pub_item_mod.attrs.into_iter().filter_map(|x| {
+        log::info!("{:?}", &x);
+        if x.style != AttrStyle::Outer {
+            return Some(x);
+        }
+
+        match x.parse_meta() {
+            Ok(meta) => match meta {
+                Meta::NameValue(m) if (m.path.get_ident()?.to_string() == "doc") => {
+                    match m.lit {
+                        Lit::Str(lit_str) => {
+                            doc_input.push(lit_str.value().trim().to_string());
+                            None
+                        }
+                        _ => Some(x)
+                    }
+                }
+                _ => Some(x)
+            }
+            _ => Some(x)
+        }
+    }).collect();
+
+    if doc_input.len() >= 1 {
+        doc_input[0] = format!("{} {}", doc_input[0], SCRIPT_MOD_FN);
+    } else {
+        doc_input.push(SCRIPT_MOD_FN.to_string());
+    }
+
+    let doc_input = doc_input.join("\n");
+    log::info!("{}", &doc_input);
+
+    let doc_input = format!("{}\n\n{}", doc_input, SCRIPT_MOD);
+
+    Ok(quote! {
+        #[cfg(any(feature = "unstable", doc))]
+        #[cfg_attr(doc, deprecated(note = "hello"))]
+        #[doc = #doc_input]
+        #pub_item_mod
+
+        #[cfg(all(not(feature = "unstable"), not(doc)))]
+        #item_mod
+    })
+}
 
 fn unstable_item_fn(item_fn: ItemFn) -> Result<TokenStream, syn::Error> {
     match item_fn.vis {
@@ -14,38 +130,46 @@ fn unstable_item_fn(item_fn: ItemFn) -> Result<TokenStream, syn::Error> {
     let mut pub_item_fn = item_fn.clone();
     pub_item_fn.vis = Visibility::Public(syn::VisPublic { pub_token: syn::token::Pub::default() });
 
+    let mut doc_input: Vec<String> = vec![];
+
+    pub_item_fn.attrs = pub_item_fn.attrs.into_iter().filter_map(|x| {
+        log::info!("{:?}", &x);
+        if x.style != AttrStyle::Outer {
+            return Some(x);
+        }
+
+        match x.parse_meta() {
+            Ok(meta) => match meta {
+                Meta::NameValue(m) if (m.path.get_ident()?.to_string() == "doc") => {
+                    match m.lit {
+                        Lit::Str(lit_str) => {
+                            doc_input.push(lit_str.value().trim().to_string());
+                            None
+                        }
+                        _ => Some(x)
+                    }
+                }
+                _ => Some(x)
+            }
+            _ => Some(x)
+        }
+    }).collect();
+
+    if doc_input.len() >= 1 {
+        doc_input[0] = format!("{} {}", doc_input[0], SCRIPT_MOD_FN);
+    } else {
+        doc_input.push(SCRIPT_MOD_FN.to_string());
+    }
+
+    let doc_input = doc_input.join("\n");
+    log::info!("{}", &doc_input);
+
+    let doc_input = format!("{}\n\n{}", doc_input, SCRIPT_FN);
+
     Ok(quote! {
         #[cfg(any(feature = "unstable", doc))]
         #[cfg_attr(doc, deprecated(note = "hello"))]
-        /// <script>
-        /// if (document.body.classList.contains("mod")) {
-        ///   var s = document.currentScript
-        ///   var n = s.parentNode.previousElementSibling
-        ///   n.classList.remove("deprecated")
-        ///   n.classList.add("unstable")
-        ///   n.innerText = "Unstable"
-        ///   s.parentNode.insertBefore(document.createTextNode("This is some text"), s)
-        /// }
-        /// </script>
-        /// This is some text
-        /// <div class="stability export-unstable">
-        /// <div class="stab unstable">
-        /// <details>
-        /// <summary>
-        /// <span class="emoji">🔬</span>
-        /// This function is <strong>unstable</strong>, and requires the feature <code>unstable</code> to be enabled.
-        /// </summary></details></div></div>
-        /// <script>
-        /// (function(){
-        /// if (document.body.classList.contains("fn")) {
-        ///   var s = document.currentScript
-        ///   var u = s.previousElementSibling
-        ///   var x = s.parentNode.previousElementSibling
-        ///   console.log(u, x)
-        ///   x.parentNode.replaceChild(u, x)
-        /// }
-        /// })()
-        /// </script>
+        #[doc = #doc_input]
         #pub_item_fn
 
         #[cfg(all(not(feature = "unstable"), not(doc)))]
@@ -58,6 +182,7 @@ pub fn unstable(_attr: TokenStream, item: TokenStream) -> Result<TokenStream, sy
 
     match item {
         Item::Fn(item_fn) => unstable_item_fn(item_fn),
+        Item::Mod(item_mod) => unstable_item_mod(item_mod),
         x => {
             Err(Error::new(x.span(), "not supported in this position"))
         }
--- export_test/src/lib.rs
+++ export_test/src/lib.rs
@@ -1,5 +1,22 @@
-
 #[export::unstable]
+/// Have some docs
+///
+/// Have some anger
 fn lolwut() {
     println!("yes");
+}
+
+pub fn lolwut2() {
+    println!("yes");
+}
+
+#[export::unstable]
+fn lolwut3() {
+    println!("yes");
+}
+
+#[export::unstable]
+mod lolmod {
+    #[export::unstable]
+    fn lolwut4() {}
 }
